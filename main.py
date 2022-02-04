19867aeed6b049032ae4861f0b67ab779027de2b
Vitor Silva <vitor.azevedo009@gmail.com>
2022-02-03 21:26:07 -0300

    parameters in json format pratice

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+from pydantic import BaseModel
+
 from fastapi import FastAPI
+from fastapi.param_functions import Body
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
 @app.get("/")
-async def root():
+def get_users():
     """root."""
     return {"message": "Hello World"}
 
+
+@app.get("/posts")
+def get_posts():
+    """show all posts"""
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    """create post"""
+    return {"new_post": f"title: {new_post.title}, content: {new_post.content}"}
